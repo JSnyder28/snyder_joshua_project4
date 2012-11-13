326c645e20f0c6efc607bb688694ab6e76164c20
Joshua Snyder <jsnyder28@fullsail.edu>
2012-11-13 16:29:11 -0600

    Created static data to be displayed in the form fields.

---
+++ js/json.js
@@ -0,0 +1,50 @@
+var json = {
+	"Recipe1": {
+		"cat": ["Category:", "American"],
+		"date": ["Date Added", "2012-11-13"],
+		"rcpName": ["Recipe Name:", "Cheeseburger"],
+		"directions": ["Directions:", "Season the beef and grill until done."],
+		"favorite": ["Go-To-Meal?:", "Yes"],
+		"rating": ["Rating:", "4"]
+	},
+	"Recipe2": {
+		"cat": ["Category:", "Chinese"],
+		"date": ["Date Added", "2012-10-13"],
+		"rcpName": ["Recipe Name:", "Broccoli Beef"],
+		"directions": ["Directions:", "Stirfry the vegetables and add seared beef."],
+		"favorite": ["Go-To-Meal?:", "Yes"],
+		"rating": ["Rating:", "5"]
+	},
+	"Recipe3": {
+		"cat": ["Category:", "Italian"],
+		"date": ["Date Added", "2012-11-10"],
+		"rcpName": ["Recipe Name:", "Meatball Sub"],
+		"directions": ["Directions:", "Make meatballs, add sauce, and put on bread of choice."],
+		"favorite": ["Go-To-Meal?:", "No"],
+		"rating": ["Rating:", "3"]
+	},
+	"Recipe4": {
+		"cat": ["Category:", "Japanese"],
+		"date": ["Date Added", "2012-12-17"],
+		"rcpName": ["Recipe Name:", "Sushi"],
+		"directions": ["Directions:", "Roll desired fish in Salmon skin."],
+		"favorite": ["Go-To-Meal?:", "No"],
+		"rating": ["Rating:", "1"]
+	},
+	"Recipe5": {
+		"cat": ["Category:", "Mexican"],
+		"date": ["Date Added", "2012-11-5"],
+		"rcpName": ["Recipe Name:", "Enchiladas"],
+		"directions": ["Directions:", "Bake in the oven for 35 minutes on 375."],
+		"favorite": ["Go-To-Meal?:", "Yes"],
+		"rating": ["Rating:", "2"]
+	},
+	"Recipe6": {
+		"cat": ["Category:", "Seasonal"],
+		"date": ["Date Added", "2012-10-26"],
+		"rcpName": ["Recipe Name:", "Duck"],
+		"directions": ["Directions:", "Smoke for best seasonal flavor."],
+		"favorite": ["Go-To-Meal?:", "No"],
+		"rating": ["Rating:", "5"]
+	}
+}
