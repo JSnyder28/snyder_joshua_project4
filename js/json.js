ae1350aae0b7432d3165b5714657c4fb82b1daf3
Joshua Snyder <jsnyder28@fullsail.edu>
2012-11-14 18:29:02 -0600

    Added a colon the the Date Added item.

--- js/json.js
+++ js/json.js
@@ -1,7 +1,7 @@
 var json = {
 	"Recipe1": {
 		"cat": ["Category:", "American"],
-		"date": ["Date Added", "2012-11-13"],
+		"date": ["Date Added:", "2012-11-13"],
 		"rcpName": ["Recipe Name:", "Cheeseburger"],
 		"directions": ["Directions:", "Season the beef and grill until done."],
 		"favorite": ["Go-To-Meal?:", "Yes"],
@@ -9,7 +9,7 @@ var json = {
 	},
 	"Recipe2": {
 		"cat": ["Category:", "Chinese"],
-		"date": ["Date Added", "2012-10-13"],
+		"date": ["Date Added:", "2012-10-13"],
 		"rcpName": ["Recipe Name:", "Broccoli Beef"],
 		"directions": ["Directions:", "Stirfry the vegetables and add seared beef."],
 		"favorite": ["Go-To-Meal?:", "Yes"],
@@ -17,7 +17,7 @@ var json = {
 	},
 	"Recipe3": {
 		"cat": ["Category:", "Italian"],
-		"date": ["Date Added", "2012-11-10"],
+		"date": ["Date Added:", "2012-11-10"],
 		"rcpName": ["Recipe Name:", "Meatball Sub"],
 		"directions": ["Directions:", "Make meatballs, add sauce, and put on bread of choice."],
 		"favorite": ["Go-To-Meal?:", "No"],
@@ -25,7 +25,7 @@ var json = {
 	},
 	"Recipe4": {
 		"cat": ["Category:", "Japanese"],
-		"date": ["Date Added", "2012-12-17"],
+		"date": ["Date Added:", "2012-12-17"],
 		"rcpName": ["Recipe Name:", "Sushi"],
 		"directions": ["Directions:", "Roll desired fish in Salmon skin."],
 		"favorite": ["Go-To-Meal?:", "No"],
@@ -33,7 +33,7 @@ var json = {
 	},
 	"Recipe5": {
 		"cat": ["Category:", "Mexican"],
-		"date": ["Date Added", "2012-11-5"],
+		"date": ["Date Added:", "2012-11-5"],
 		"rcpName": ["Recipe Name:", "Enchiladas"],
 		"directions": ["Directions:", "Bake in the oven for 35 minutes on 375."],
 		"favorite": ["Go-To-Meal?:", "Yes"],
@@ -41,7 +41,7 @@ var json = {
 	},
 	"Recipe6": {
 		"cat": ["Category:", "Seasonal"],
-		"date": ["Date Added", "2012-10-26"],
+		"date": ["Date Added:", "2012-10-26"],
 		"rcpName": ["Recipe Name:", "Duck"],
 		"directions": ["Directions:", "Smoke for best seasonal flavor."],
 		"favorite": ["Go-To-Meal?:", "No"],
